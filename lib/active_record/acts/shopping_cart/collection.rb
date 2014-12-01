7e7283f01ce3fca1bd5177cb457a40b9cebd788d
Javier Ayala <javier@healthloop.com>
2014-11-30 18:34:25 -0800

    changed the behavior to have multiple items of the same object with different properties like the size

--- lib/active_record/acts/shopping_cart/collection.rb
+++ lib/active_record/acts/shopping_cart/collection.rb
@@ -6,16 +6,7 @@ module ActiveRecord
         # Adds a product to the cart
         #
         def add(object, price, quantity = 1, size = "N/A", cumulative = true)
-          cart_item = item_for(object)
-
-          if cart_item
-            cumulative = cumulative == true ? cart_item.quantity : 0
-            cart_item.quantity = (cumulative + quantity)
-            cart_item.save
-            cart_item
-          else
-            shopping_cart_items.create(item: object, price: price, quantity: quantity, size: size)
-          end
+          shopping_cart_items.create(item: object, price: price, quantity: quantity, size: size)
         end
 
         #
